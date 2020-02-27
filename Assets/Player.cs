1c0366084e16e4e9b3f0bd4c0a20a765f0ff0eaf
har-y <jakub.harenda@gmail.com>
2020-02-27 22:32:57 +0100

    Add: Player - Axis Limit Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed = 8f;
 
+    private float _xTilt = 2.25f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,5 +19,8 @@ public class Player : MonoBehaviour
     {
         float _horizontal = Input.GetAxis("Horizontal");
         transform.Translate(_horizontal * Time.deltaTime * _playerSpeed, 0f, 0f);
+
+        float _positionXRange = Mathf.Clamp(transform.position.x, -_xTilt, _xTilt);
+        transform.position = new Vector3(_positionXRange, transform.position.y, transform.position.z);
     }
 }
