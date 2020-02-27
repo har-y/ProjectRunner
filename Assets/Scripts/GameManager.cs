22249e97e2e866797001cef23bb27f830e686d24
har-y <jakub.harenda@gmail.com>
2020-02-27 23:24:37 +0100

    Add: Game Manager - Spawn Enemies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _enemyObject;
+   
+    private float _xTilt = 1.75f;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("SpawnEnemyRoutine");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnEnemy()
+    {
+        float _randomValueX = Random.Range(-_xTilt, _xTilt);
+
+        Vector3 _enemyPosition = new Vector3(_randomValueX, 0.5f, 80f);
+
+        Instantiate(_enemyObject, _enemyPosition, Quaternion.identity);
+    }
+
+    private IEnumerator SpawnEnemyRoutine()
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
