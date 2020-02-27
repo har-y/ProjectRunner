8483673f42dbba7c0f03583fbe9d5b0179a87ba1
har-y <jakub.harenda@gmail.com>
2020-02-27 22:30:04 +0100

    Add: Player - Horizontal Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _playerSpeed = 8f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float _horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(_horizontal * Time.deltaTime * _playerSpeed, 0f, 0f);
     }
 }
